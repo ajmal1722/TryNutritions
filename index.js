5f79e51fb118beabd47c82a2ecab6ad7f3e4fe72
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-01-30 20:07:39 +0530

    set view engines and loaded assets

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 8080
+
+// log requests
+app.use(morgan('tiny'));
+
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// set view engine
+app.set('view engine', 'ejs');
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname,'assets/css')));
+app.use('/js',express.static(path.resolve(__dirname,'assets/js')));
+app.use('/img',express.static(path.resolve(__dirname,'assets/img')));
+
+app.get('/',(req, res) => {
+    res.send('TryNutritions')
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`)
+})
