d49757acad8790b3127b8e6f48ed81fc231f8804
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-05 19:51:00 +0530

    generated jwt token for user when signup

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,8 +1,11 @@
 const Userdb = require('../model/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // create and save user
 exports.create = async (req, res) => {
     try {
+        // get all data from body
         const data = {
             name: req.body.name,
             phone: req.body.phone,
@@ -22,10 +25,29 @@ exports.create = async (req, res) => {
             return res.status(400).send('Email already exists');
         }
 
-        // Insert new user data
-        const userData = await Userdb.create(data);
+        // encrypt the password
+        const encPassword = await bcrypt.hash(data.password, 10) // here 10 is optional
 
-        res.status(201).send('Data added successfully');
+        // Insert new user data
+        const userData = await Userdb.create({
+            ...data,
+            password: encPassword
+        });
+
+        // generate a token for user
+        const token = jwt.sign(
+            {id: userData._id, email: userData.email},
+            'shhhh',
+            {
+                expiresIn: '2h'
+            }
+        );
+
+        userData.token = token
+        userData.password = undefined
+        
+        res.status(201).json(userData);
+
     } catch (error) {
         console.error('Error message:', error.message);
         res.status(500).send({
