98b5d3a47e3dff6d6716099526177b08b6fa80c2
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-12 16:14:04 +0530

    added category page and set routes products, banner etc

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -9,6 +9,22 @@ const services = require('../services/adminRender')
 // admin dashboard
 route.get('/admin/', services.admindashboard);
 
+// category
+route.get('/category', services.category)
+
+// orders
 route.get('/orders', services.orders);
+
+route.get('/products', services.products);
+
+route.get('/coupons', services.coupons);
+
+route.get('/banners', services.banners);
+
+route.get('/payments', services.payments);
+
+route.get('/settings', services.settings);
+
+route.get('/products', services.products);
 
 module.exports = route;
--- server/services/adminRender.js
+++ server/services/adminRender.js
@@ -1,5 +1,18 @@
 // admin dashboard
-exports.admindashboard = (req, res) => res.render('admin/body/dashboard.ejs');
+exports.admindashboard = (req, res) => res.render('admin/body/dashboard');
 
 // orders
-exports.orders = (req, res) => res.render('admin/body/orders.ejs')
+exports.orders = (req, res) => res.render('admin/body/orders')
+
+// category
+exports.category = (req, res) => res.render('admin/body/category');
+
+exports.products = (req, res) => res.render('admin/body/products');
+
+exports.coupons = (req, res) => res.render('admin/body/coupons');
+
+exports.banners = (req, res) => res.render('admin/body/banner');
+
+exports.payments = (req, res) => res.render('admin/body/payments');
+
+exports.settings = (req, res) => res.render('admin/body/settings');
