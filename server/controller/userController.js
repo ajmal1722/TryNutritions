5f2f169eb9f7d6d50184c5243125481cc008e95e
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-02-28 19:58:11 +0530

    sorted recommended products in home page based on time

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -9,20 +9,26 @@ const Products = require('../model/products');
 exports.homeRoutes = async (req, res) => {
     const category = await Category.find({}).exec();
     const product = await Products.find({}).exec();
+    const latestProduct = await Products.find({})
+                                .sort({createdAt: -1})
+                                .limit(8)
+                                .exec()
     try {
         const verify = jwt.verify(req.cookies.jwt, 'shhhh');
         
         res.status(201).render('user/body/home', {
             username: verify.name,
             Categories: category,
-            Products: product
+            Products: product,
+            latestProducts: latestProduct
         })
     } catch (error) {
         if (error.name === 'TokenExpiredError' || 'JsonWebTokenError') {
             res.status(201).render('user/body/home', {
                 username: undefined, 
                 Categories: category,
-                Products: product
+                Products: product,
+                latestProducts: latestProduct
             });
         } else {
             res.status(500).send(error);
