dee82b0828d5199814f7cfddb5f8be900d77b03a
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-03-16 14:42:23 +0530

    add styles to checkout page

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,5 +1,26 @@
 const mongoose = require('mongoose');
 
+const addressSchema = new mongoose.Schema({
+    houseName: {
+        type: String
+    },
+    streetName: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    district: {
+        type: String
+    },
+    state: {
+        type: String
+    },
+    pincode: {
+        type: Number
+    }
+});
+
 const Schema = new mongoose.Schema({
     name: {
         type: String,
@@ -20,7 +41,8 @@ const Schema = new mongoose.Schema({
     isBlocked: {
         type: String,
         default: 'Active'
-    }  
+    },
+    addresses: [addressSchema] 
 })
 
 const Userdb = mongoose.model('users', Schema);
